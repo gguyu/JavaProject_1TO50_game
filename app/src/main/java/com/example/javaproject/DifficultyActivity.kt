7a0c0b0b0e56a17a07bdd9f8a68b9facb1b220b7
ggyutato <ghkdrbwls99@naver.com>
2021-11-27 22:48:24 +0900

    title add

--- app/src/main/java/com/example/javaproject/DifficultyActivity.kt
+++ app/src/main/java/com/example/javaproject/DifficultyActivity.kt
@@ -6,10 +6,31 @@ import android.widget.Button
 
 class DifficultyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var easyButton : Button
+        lateinit var commonButton : Button
+        lateinit var hardButton : Button
         lateinit var homeButton : Button
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_difficulty)
+
+        // 이지 버튼 ==> 누르면 쉬움 난이도로 게임 실행
+        easyButton = findViewById(R.id.easyButton)
+        easyButton.setOnClickListener {
+            println("쉬움 모드, 난이도를 인자로 넘겨주기")
+        }
+
+        // 커먼 버튼 ==> 누르면 보통 난이도로 게임 실행
+        commonButton = findViewById(R.id.commonButton)
+        commonButton.setOnClickListener {
+            println("보통 모드, 난이도를 인자로 넘겨주기")
+        }
+
+        // 커먼 버튼 ==> 누르면 보통 난이도로 게임 실행
+        hardButton = findViewById(R.id.hardButton)
+        hardButton.setOnClickListener {
+            println("하드 모드, 난이도를 인자로 넘겨주기")
+        }
 
         // 홈 버튼 ==> 누르면 타이틀 화면으로 다시 이동
         homeButton = findViewById(R.id.homeButton)
